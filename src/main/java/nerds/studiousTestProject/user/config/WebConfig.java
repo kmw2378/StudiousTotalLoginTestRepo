a3af1cb4f84e995c8e79c6a701ddf0461b026c77
kmw2378 <rlarla677@gmail.com>
2023-06-18 19:27:13 +0900

    refactor: 기존 cors 관련 설정사항인 WebConfig 수정

--- src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
+++ src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
@@ -9,11 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")    // 외부에서 들어오는 모든 url 을 허용
+//                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")   // 허용되는 Method
+                .allowedOriginPatterns("*") // 외부에서 들어오는 모든 url 을 허용
                 .allowedHeaders("*")    // 허용되는 헤더
                 .exposedHeaders("*")    // response의 모든 헤더 허용
-                .allowCredentials(false)    // 자격증명 비허용, 쿠키 허용
+                .allowCredentials(true)    // 자격증명 허용
                 .maxAge(3600);   // 허용 시간
     }
 }
