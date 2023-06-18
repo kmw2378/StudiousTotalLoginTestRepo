3548052f3241bddc0b7ad18ec9fc8257cc79bfb6
kmw2378 <rlarla677@gmail.com>
2023-06-18 19:35:22 +0900

    refactor: 기존 cors 관련 설정사항인 WebConfig 수정

--- src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
+++ src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")   // 허용되는 Method
                 .allowedHeaders("*")    // 허용되는 헤더
-                .exposedHeaders("*")    // response의 모든 헤더 허용
-                .allowCredentials(true)    // 자격증명 허용
+//                .exposedHeaders("*")    // response의 모든 헤더 허용
+//                .allowCredentials(true)    // 자격증명 허용
                 .maxAge(3600);   // 허용 시간
     }
 }
