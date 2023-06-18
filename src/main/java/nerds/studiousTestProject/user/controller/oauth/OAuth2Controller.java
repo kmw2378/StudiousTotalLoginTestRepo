b72d4a38f453ebff1dd9eee895372f1e2a721629
kmw2378 <rlarla677@gmail.com>
2023-06-18 15:59:36 +0900

    feat: 토큰 발급 Controller 에 ACCESS_CONTROL_ALLOW_ORIGIN 헤더 추가 - 카카오 토큰 발급 과정에서 프론트엔드 서버로 forwarding 시 엑세스 거부되는 현상을 해결하기 위해 응답에 헤더 추가

--- src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
+++ src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
@@ -1,9 +1,11 @@
 package nerds.studiousTestProject.user.controller.oauth;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nerds.studiousTestProject.user.dto.oauth.token.TokenInfo;
 import nerds.studiousTestProject.user.service.oauth.OAuth2Service;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +20,8 @@ public class OAuth2Controller {
     private final OAuth2Service oAuth2Service;
 
     @GetMapping("/authorize/{provider}")
-    public TokenInfo authorize(@PathVariable String provider, @RequestParam String code) {
+    public TokenInfo authorize(@PathVariable String provider, @RequestParam String code, HttpServletResponse response) {
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
         return oAuth2Service.authorize(provider, code);
     }
 }
