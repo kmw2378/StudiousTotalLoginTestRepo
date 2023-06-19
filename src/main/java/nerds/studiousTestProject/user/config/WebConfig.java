f902ebcfde8d284c6354a2a6d24d13db3bc60b6e
kmw2378 <rlarla677@gmail.com>
2023-06-19 13:57:44 +0900

    refactor: 다른 uri은 403 오류 발생하도록 수정

--- src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
+++ src/main/java/nerds/studiousTestProject/user/config/WebConfig.java
@@ -10,15 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://ec2-54-180-201-100.ap-northeast-2.compute.amazonaws.com:8080",
-                        "http://localhost:3000",
-                        "https://localhost:3000",
-                        "https://127.0.0.1:3000"
+                .allowedOriginPatterns(
+                        "http://localhost:8080",
+                        "http://ec2-54-180-201-100.ap-northeast-2.compute.amazonaws.com:8080",
+                        "http://localhost:3000"
                 )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")   // 허용되는 Method
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")   // 허용되는 Method
                 .allowedHeaders("*")    // 허용되는 헤더
-//                .exposedHeaders("*")    // response의 모든 헤더 허용
-//                .allowCredentials(true)    // 자격증명 허용
+                .exposedHeaders("*")    // response의 모든 헤더 허용
+                .allowCredentials(true)    // 자격증명 허용
                 .maxAge(3600);   // 허용 시간
     }
 }
