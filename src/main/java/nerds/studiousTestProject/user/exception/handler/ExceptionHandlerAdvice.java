18b28de3cedf35657f8d6f65f5e6b34bf6277a21
kmw2378 <rlarla677@gmail.com>
2023-06-19 15:11:54 +0900

    feat: ExceptionHandlerAdvice 추가 - 예외 발생 시 처리해주는 핸들러 추가

---
+++ src/main/java/nerds/studiousTestProject/user/exception/handler/ExceptionHandlerAdvice.java
@@ -0,0 +1,23 @@
+package nerds.studiousTestProject.user.exception.handler;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.user.exception.dto.ExceptionDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionHandlerAdvice {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ExceptionDto> exceptionHandler(Exception exception, HttpServletResponse response) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionDto.builder()
+                        .message(String.format("%s", exception.getMessage()))
+                        .statusCode(response.getStatus())
+                        .build()
+                );
+    }
+}
