d89622e0b176a27b0eed4538e22a981b7c3271a0
kmw2378 <rlarla677@gmail.com>
2023-06-19 15:12:28 +0900

    feat: ExceptionMessage 추가 - 예외 발생 시 메시지(상수값)를 저장하고 있는 클래스 추가

---
+++ src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
@@ -0,0 +1,27 @@
+package nerds.studiousTestProject.user.exception.message;
+
+public enum ExceptionMessage {
+    MISMATCH_EMAIL("이메일이 일치하지 않습니다."),
+    MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다."),
+    ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
+    LOGOUT_USER("로그아웃된 회원입니다."),
+    MISMATCH_USERNAME_TOKEN("아이디와 토큰값이 일치하지 않습니다."),
+    NOT_AUTHORIZE_ACCESS("인증되지 않은 접근입니다."),
+    FAIL_TOKEN_CHECK("토큰 검증에 실패했습니다."),
+    INVALID_TOKEN("유효하지 않은 토큰입니다."),
+    TOKEN_VALID_TIME_EXPIRED("토큰의 유효기간이 만료되었습니다."),
+    TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다."),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다."),
+    MISMATCH_TOKEN("토큰명이 일치하지 않습니다."),
+    NOT_SUPPORTED_JWT("JWT 토큰이 지원하지 않습니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
