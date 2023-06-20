2f8407ff8c8ba15668d7cac0744f284226bb7b8e
kmw2378 <rlarla677@gmail.com>
2023-06-20 16:02:32 +0900

    feat: OAuth2Token 에 @Builder 어노테이션 추가

--- src/main/java/nerds/studiousTestProject/user/auth/oauth/OAuth2Token.java
+++ src/main/java/nerds/studiousTestProject/user/auth/oauth/OAuth2Token.java
@@ -1,6 +1,7 @@
 package nerds.studiousTestProject.user.auth.oauth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OAuth2Token {
