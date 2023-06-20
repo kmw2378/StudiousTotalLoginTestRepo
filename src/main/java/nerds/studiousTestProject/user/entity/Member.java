1e3367eb9fb57bb1925725ee14767a908393ef11
kmw2378 <rlarla677@gmail.com>
2023-06-20 21:23:31 +0900

    remove: Member 엔티티에 기존 OAuth2 객체 관련 연관관계 제거

--- src/main/java/nerds/studiousTestProject/user/entity/Member.java
+++ src/main/java/nerds/studiousTestProject/user/entity/Member.java
@@ -1,19 +1,14 @@
 package nerds.studiousTestProject.user.entity;
 
-import io.jsonwebtoken.lang.Assert;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import nerds.studiousTestProject.user.auth.oauth.account.OAuth2Account;
 import nerds.studiousTestProject.user.dto.general.MemberSignUpResponse;
 import nerds.studiousTestProject.user.dto.general.MemberType;
 import org.springframework.security.core.GrantedAuthority;
@@ -44,10 +39,6 @@ public class Member implements UserDetails {
     private List<String> roles = new ArrayList<>();
 
     private MemberType type;
-
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "SOCIAL_ID")
-    private OAuth2Account social;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -100,18 +91,5 @@ public class Member implements UserDetails {
                 .password(memberSignUpResponse.getPassword())
                 .roles(memberSignUpResponse.getRoles())
                 .build();
-    }
-
-    public void linkSocial(OAuth2Account oAuth2Account) {
-        Assert.state(social == null, "하나의 소셜 서비스만 연동할 수 있습니다.");
-        this.social = oAuth2Account;
-        oAuth2Account.linkUser(this);
-    }
-
-    public void unlinkSocial() {
-        Assert.state(type.equals(MemberType.DEFAULT), "소셜 계정으로 가입된 계정은 연동 해제가 불가능합니다.");
-        Assert.state(social != null, "연동된 소셜 계정 정보가 없습니다.");
-        this.social.unlinkUser();
-        this.social = null;
     }
 }
