ed192741d2224ff46e0452bf09f8ea0ebc8b1f26
kmw2378 <rlarla677@gmail.com>
2023-06-20 22:40:58 +0900

    feat: OAuth2Token 엔티티화 - OAuth 를 통한 로그인에서 소셜 서버에서 발급받는 토큰을 저장하는 클래스를 DTO에서 엔티티로 변경

---
+++ src/main/java/nerds/studiousTestProject/user/entity/oauth/OAuth2Token.java
@@ -0,0 +1,35 @@
+package nerds.studiousTestProject.user.entity.oauth;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nerds.studiousTestProject.user.entity.Member;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class OAuth2Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String accessToken;
+    private String refreshToken;
+    private LocalDateTime expiredAt;
+
+    @OneToOne(mappedBy = "oAuth2Token")
+    private Member member;
+}
