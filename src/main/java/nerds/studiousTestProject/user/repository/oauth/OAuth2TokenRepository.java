acbd2b31c60c855832133246f25ed3757ac1d2ba
kmw2378 <rlarla677@gmail.com>
2023-06-20 22:40:30 +0900

    feat: OAuth2TokenRepository 추가 - OAuth 를 통한 로그인에서 소셜 서버에서 발급받는 토큰을 저장하는 Repository 생성

---
+++ src/main/java/nerds/studiousTestProject/user/repository/oauth/OAuth2TokenRepository.java
@@ -0,0 +1,10 @@
+package nerds.studiousTestProject.user.repository.oauth;
+
+import nerds.studiousTestProject.user.entity.oauth.OAuth2Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OAuth2TokenRepository extends JpaRepository<OAuth2Token, String> {
+    Optional<OAuth2Token> findByEmail(String email);
+}
