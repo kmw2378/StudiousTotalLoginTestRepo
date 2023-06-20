29759a0bc3053780bb85685e4fa4315f4e2cbc62
kmw2378 <rlarla677@gmail.com>
2023-06-20 14:13:39 +0900

    feat: code 로그 추가

--- src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
+++ src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
@@ -1,11 +1,9 @@
 package nerds.studiousTestProject.user.controller.oauth;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nerds.studiousTestProject.user.dto.oauth.token.TokenInfo;
+import nerds.studiousTestProject.user.dto.general.token.JwtTokenResponse;
 import nerds.studiousTestProject.user.service.oauth.OAuth2Service;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +18,8 @@ public class OAuth2Controller {
     private final OAuth2Service oAuth2Service;
 
     @GetMapping("/authorize/{provider}")
-    public TokenInfo authorize(@PathVariable String provider, @RequestParam String code) {
+    public JwtTokenResponse authorize(@PathVariable String provider, @RequestParam String code) {
+        log.info("code = {}", code);
         return oAuth2Service.authorize(provider, code);
     }
 }
