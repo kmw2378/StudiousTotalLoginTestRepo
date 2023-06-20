4412af414b12d8335641d8faed3dbcc0daf4a153
kmw2378 <rlarla677@gmail.com>
2023-06-20 22:42:54 +0900

    refactor: 소셜 서버로 부터 로그아웃 요청 시 응답값을 받을 클래스 추가

---
+++ src/main/java/nerds/studiousTestProject/user/dto/oauth/OAuth2LogoutResponse.java
@@ -0,0 +1,12 @@
+package nerds.studiousTestProject.user.dto.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class OAuth2LogoutResponse {
+    private String id;
+}
