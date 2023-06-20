83e8cecc79d258c54b5793c48106e18d4a696749
kmw2378 <rlarla677@gmail.com>
2023-06-21 01:30:32 +0900

    refactor: @Controller -> @RestController 로 수정

--- src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
+++ src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
