2cef34cc456c2f9855d4ec9931e970783a8d9f88
kmw2378 <rlarla677@gmail.com>
2023-06-21 15:00:19 +0900

    feat: 기능 추가 - email 정보가 null 일 시 UUID 를 통해 이메일 정보를 받도록 수정 - 기존 Kakao 전용 토큰 응답 정보 DTO 대신 OAuth2TokenRequest, Response 로 대체

--- src/main/java/nerds/studiousTestProject/user/service/oauth/OAuth2Service.java
+++ src/main/java/nerds/studiousTestProject/user/service/oauth/OAuth2Service.java
@@ -3,13 +3,13 @@ package nerds.studiousTestProject.user.service.oauth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nerds.studiousTestProject.user.auth.oauth.userinfo.OAuth2UserInfo;
-import nerds.studiousTestProject.user.auth.oauth.userinfo.OAuth2UserInfoFactory;
+import nerds.studiousTestProject.user.dto.oauth.token.OAuth2TokenRequest;
+import nerds.studiousTestProject.user.dto.oauth.token.OAuth2TokenResponse;
+import nerds.studiousTestProject.user.dto.oauth.userinfo.OAuth2UserInfo;
+import nerds.studiousTestProject.user.dto.oauth.userinfo.OAuth2UserInfoFactory;
 import nerds.studiousTestProject.user.dto.general.MemberType;
 import nerds.studiousTestProject.user.dto.general.token.JwtTokenResponse;
-import nerds.studiousTestProject.user.dto.oauth.KakaoTokenRequest;
-import nerds.studiousTestProject.user.dto.oauth.KakaoTokenResponse;
-import nerds.studiousTestProject.user.dto.oauth.OAuth2LogoutResponse;
+import nerds.studiousTestProject.user.dto.oauth.token.OAuth2LogoutResponse;
 import nerds.studiousTestProject.user.entity.Member;
 import nerds.studiousTestProject.user.entity.oauth.OAuth2Token;
 import nerds.studiousTestProject.user.entity.token.LogoutAccessToken;
@@ -62,11 +62,11 @@ public class OAuth2Service {
 
         // 토큰 받아오기
         // 이는 실제 사용할 토큰이 아닌 유저 정보를 가져오기 위한 토큰 정보이다.
-        KakaoTokenResponse kakaoTokenResponse = getSocialToken(code, provider);
-        log.debug("token = {}", kakaoTokenResponse.toString());
+        OAuth2TokenResponse oAuth2TokenResponse = getSocialToken(code, provider);
+        log.debug("token = {}", oAuth2TokenResponse.toString());
 
         // 소셜 엑세스 토큰을 통해 사용자 정보 받아오기
-        Map<String, Object> attributes = getUserAttributes(provider, kakaoTokenResponse);
+        Map<String, Object> attributes = getUserAttributes(provider, oAuth2TokenResponse);
         log.info("attributes = {}", attributes);
 
         // 팩토리 클래스를 통해 구글, 네이버, 카카오 중 알맞는 소셜 사용자 정보를 가져온다.
@@ -74,7 +74,8 @@ public class OAuth2Service {
         log.info("userInfo = {}", oAuth2UserInfo.toString());
 
         // 유저 정보를 통해 이메일, 비밀번호 생성 (이 때, 비밀번호는 UUID 를 통해 랜덤으로 생성)
-        String email = oAuth2UserInfo.getEmail() == null ? UUID.randomUUID() + "@kakao.com" : oAuth2UserInfo.getEmail(); // 이 값이 null 이 되버림
+        // 이메일이 안넘어오는 경우 (사용자가 동의 X) 는 UUID를 사용하여 이메일 생성
+        String email = oAuth2UserInfo.getEmail();
         String password = UUID.randomUUID().toString();
 
         // 토큰을 만들기 전 Repository 에 있는지 여부를 확인 후 이를 완료하고 진행하자.
@@ -101,9 +102,9 @@ public class OAuth2Service {
 
         OAuth2Token oAuth2Token = OAuth2Token.builder()
                 .email(email)
-                .accessToken(kakaoTokenResponse.getAccess_token())
-                .refreshToken(kakaoTokenResponse.getRefresh_token())
-                .expiredAt(DateConverter.toLocalDateTime(kakaoTokenResponse.getExpires_in()))
+                .accessToken(oAuth2TokenResponse.getAccess_token())
+                .refreshToken(oAuth2TokenResponse.getRefresh_token())
+                .expiredAt(DateConverter.toLocalDateTime(oAuth2TokenResponse.getExpires_in()))
                 .build();
         log.info("oAuth2Token = {}", oAuth2Token);
 
@@ -181,16 +182,16 @@ public class OAuth2Service {
      * @param provider 소셜에 알맞는 Provider (redirect_uri, token_uri 등..)
      * @return 소셜 서버로 부터 넘겨 받은 토큰 값들을 담은 객체
      */
-    private KakaoTokenResponse getSocialToken(String code, ClientRegistration provider) {
-        KakaoTokenResponse kakaoTokenResponse = null;
+    private OAuth2TokenResponse getSocialToken(String code, ClientRegistration provider) {
+        OAuth2TokenResponse oAuth2TokenResponse = null;
         try {
-            kakaoTokenResponse = WebClient.create()
+            oAuth2TokenResponse = WebClient.create()
                     .post()
                     .uri(provider.getProviderDetails().getTokenUri())
                     .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                     .bodyValue(getParams(code, provider))
                     .retrieve()
-                    .bodyToMono(KakaoTokenResponse.class)
+                    .bodyToMono(OAuth2TokenResponse.class)
                     .block();
         } catch (WebClientResponseException e) {
             log.error("msg = {}", e.getMessage());
@@ -198,7 +199,7 @@ public class OAuth2Service {
             log.error("body = {}", e.getResponseBodyAsString());
         }
 
-        return kakaoTokenResponse;
+        return oAuth2TokenResponse;
     }
 
     /**
@@ -210,26 +211,27 @@ public class OAuth2Service {
     private MultiValueMap<String, String> getParams(String code, ClientRegistration provider) {
         return MultiValueMapConverter.convert(
                 new ObjectMapper(),
-                KakaoTokenRequest.builder()
+                OAuth2TokenRequest.builder()
                         .code(code)
-                        .grant_type("authorization_code")
+                        .grant_type("authorization_code")    // 추후 provider 관련 값으로 수정 예정
                         .redirect_uri(provider.getRedirectUri())
                         .client_id(provider.getClientId())
+                        .client_secret(provider.getClientSecret())
                         .build()
         );
     }
 
     /**
      * 소셜 서버로 부터 발급받은 토큰 값을 통해 해당 유저 속성을 반환하는 메소드
-     * @param provider 소셜에 알맞는 Provider (redirect_uri, token_uri 등..)
-     * @param kakaoTokenResponse 소셜 서버로 부터 발급받은 토큰 값들이 저장된 DTO
+     * @param provider            소셜에 알맞는 Provider (redirect_uri, token_uri 등..)
+     * @param oAuth2TokenResponse 소셜 서버로 부터 발급받은 토큰 값들이 저장된 DTO
      * @return 소셜 서버로 부터 발급받은 토큰의 유저 속성
      */
-    private Map<String, Object> getUserAttributes(ClientRegistration provider, KakaoTokenResponse kakaoTokenResponse) {
+    private Map<String, Object> getUserAttributes(ClientRegistration provider, OAuth2TokenResponse oAuth2TokenResponse) {
         return WebClient.create()
                 .get()
                 .uri(provider.getProviderDetails().getUserInfoEndpoint().getUri())
-                .headers(header -> header.setBearerAuth(kakaoTokenResponse.getAccess_token()))
+                .headers(header -> header.setBearerAuth(oAuth2TokenResponse.getAccess_token()))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
                 .block();
