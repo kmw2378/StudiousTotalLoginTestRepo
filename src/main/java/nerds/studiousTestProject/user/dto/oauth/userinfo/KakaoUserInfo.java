dfbb9986977e96dfe446db58e52b315d2fbdce22
kmw2378 <rlarla677@gmail.com>
2023-06-21 14:58:50 +0900

    refactor: 경로 이동

---
+++ src/main/java/nerds/studiousTestProject/user/dto/oauth/userinfo/KakaoUserInfo.java
@@ -0,0 +1,33 @@
+package nerds.studiousTestProject.user.dto.oauth.userinfo;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class KakaoUserInfo extends OAuth2UserInfo {
+    public KakaoUserInfo(Map<String, Object> attributes) {
+        super(attributes);
+
+    }
+    @Override
+    public String getId() {
+        return String.valueOf(attributes.get("id"));
+    }
+
+    @Override
+    public String getName() {
+        return (String) parsingProfile().get("nickname");
+    }
+
+    @Override
+    public String getEmail() {
+        return parsingProperties().get("email") == null ? UUID.randomUUID() + "@kakao.com" : (String) parsingProperties().get("email");
+    }
+
+    private Map<String, Object> parsingProperties() {
+        return (Map<String, Object>) attributes.get("kakao_account");
+    }
+
+    private Map<String, Object> parsingProfile() {
+        return (Map<String, Object>) parsingProperties().get("profile");
+    }
+}
