6a696d191983ba2bdecbe5e35962b053ea5c3e0f
kmw2378 <rlarla677@gmail.com>
2023-06-23 00:58:22 +0900

    refactor: findByProviderId 메소드를 제거 후 existsByProviderId 메소드 추가

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
-    Optional<Member> findByProviderId(Long providerId);
+    boolean existsByProviderId(Long providerId);
     boolean existsByEmail(String email);
 }
