ec762757f82b2348c88e5a98d442cbcaaa46a5df
kmw2378 <rlarla677@gmail.com>
2023-06-23 00:50:09 +0900

    feat: findByEmail, deleteByEmail 메소드 추가

--- src/main/java/nerds/studiousTestProject/user/repository/token/RefreshTokenRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/token/RefreshTokenRepository.java
@@ -1,11 +1,11 @@
 package nerds.studiousTestProject.user.repository.token;
 
 import nerds.studiousTestProject.user.entity.token.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByEmail(String email);
+    void deleteByEmail(String email);
 }
