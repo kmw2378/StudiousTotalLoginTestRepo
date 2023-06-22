9cb2c435137772f0f9a830d3c7fc7dd92d70583b
kmw2378 <rlarla677@gmail.com>
2023-06-23 01:31:26 +0900

    refactor: 기능 수정 및 추가 - authenticate 의 반환값이 없도록 수정 - @Transactional 어노테이션 추가 - reissue 메소드에서 refreshToken이 null인 여부를 전 과정인 refreshTokenService.findByEmail 에서 하기 때므로 불필요하기에 삭제

--- src/main/java/nerds/studiousTestProject/user/service/member/MemberService.java
+++ src/main/java/nerds/studiousTestProject/user/service/member/MemberService.java
@@ -64,11 +64,11 @@ public class MemberService {
      */
     @Transactional
     public JwtTokenResponse login(String email, String password) {
-        Member member = authenticate(email, password);
+        authenticate(email, password);
 
         // 1. 토큰 생성
         String accessToken = jwtTokenProvider.createAccessToken(email, password);
-        RefreshToken refreshToken = refreshTokenService.save(member.getEmail(), jwtTokenProvider.createRefreshToken());
+        RefreshToken refreshToken = refreshTokenService.save(email, jwtTokenProvider.createRefreshToken());
 
         // 2. 쿠키에 Refresh 토큰 등록
         jwtTokenProvider.setRefreshTokenAtCookie(refreshToken);
@@ -82,6 +82,7 @@ public class MemberService {
      * @param accessToken 사용자의 accessToken
      * @return 현재 사용자의 이메일
      */
+    @Transactional
     public String logout(String accessToken) {
         String resolvedAccessToken = jwtTokenProvider.resolveToken(accessToken);
         if (resolvedAccessToken == null) {
@@ -105,6 +106,7 @@ public class MemberService {
      * @param refreshToken 사용자로부터 넘겨 받은 refreshToken
      * @return 새로운 accessToken 이 담긴 JwtTokenResponse 객체
      */
+    @Transactional
     public JwtTokenResponse reissue(String refreshToken) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName() == null) {
@@ -115,17 +117,16 @@ public class MemberService {
         String currentEmail = authentication.getName();
         log.info("currentEmail = {}", currentEmail);
         RefreshToken redisRefreshToken = refreshTokenService.findByEmail(currentEmail);
-
-        if (redisRefreshToken == null || !refreshToken.equals(redisRefreshToken.getRefreshToken())) {
+        if (!refreshToken.equals(redisRefreshToken.getRefreshToken())) {
             log.info("refreshToken = {}", refreshToken);
-            log.info("redisRefreshToken = {}", redisRefreshToken != null ? redisRefreshToken.getRefreshToken() : "null");
+            log.info("redisRefreshToken = {}", redisRefreshToken.getRefreshToken());
             throw new TokenCheckFailException(ExceptionMessage.MISMATCH_TOKEN);
         }
 
 //        Authorization 사용하여 패스워드 가져올 때 PROTECTED 되있으므로 DB에서 사용자 내역을 가져온다.
 //        String password = userDetails.getPassword();
 //        참고 : https://djunnni.gitbook.io/springboot/2019-11-30
-//        Member member = memberRepository.findByEmail(currentEmail).get();
+//        Member member = memberRepository.findById(currentEmail).get();
 //        String password = passwordEncoder.encode(member.getPassword());
 
         return reissueToken(refreshToken, authentication);
@@ -150,7 +151,7 @@ public class MemberService {
      * @param password 사용자가 입력한 비밀번호
      * @return 알맞는 회원 정보
      */
-    private Member authenticate(String email, String password) {
+    private void authenticate(String email, String password) {
         Optional<Member> memberOptional = memberRepository.findByEmail(email);
         if (memberOptional.isEmpty()) {
             throw new UserAuthException(ExceptionMessage.USER_NOT_FOUND);
@@ -160,8 +161,6 @@ public class MemberService {
         if (!passwordEncoder.matches(password, member.getPassword())) {
             throw new UserAuthException(ExceptionMessage.MISMATCH_PASSWORD);
         }
-
-        return member;
     }
 
     /**
