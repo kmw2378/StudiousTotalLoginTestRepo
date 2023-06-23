b74e2cc7ff4ad98ab946b13e09ccec1f6c76f4ea
kmw2378 <rlarla677@gmail.com>
2023-06-23 14:36:48 +0900

    refactor: 응답 DTO 들의 어노테이션을 @Data 로 수정

--- src/main/java/nerds/studiousTestProject/user/dto/oauth/token/OAuth2TokenResponse.java
+++ src/main/java/nerds/studiousTestProject/user/dto/oauth/token/OAuth2TokenResponse.java
@@ -1,10 +1,8 @@
 package nerds.studiousTestProject.user.dto.oauth.token;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
-
 public class OAuth2TokenResponse {
     private String access_token;
     private String refresh_token;
