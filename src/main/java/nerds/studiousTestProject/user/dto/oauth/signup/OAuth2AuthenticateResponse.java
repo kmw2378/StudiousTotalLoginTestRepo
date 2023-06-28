8beeb8265e69cb413bc06848c05caa509b2fa834
kmw2378 <rlarla677@gmail.com>
2023-06-28 14:29:47 +0900

    feat: 소셜 로그인 시 응답 DTO 클래스 추가 - exist : 기존/신규 회원 여부 - JwtTokenResponse : 기존 회원일 시 토큰 - UserInfo: 신규 회원일 시 소셜 정보로 부터 가져온 회원 정보

---
+++ src/main/java/nerds/studiousTestProject/user/dto/oauth/signup/OAuth2AuthenticateResponse.java
@@ -0,0 +1,25 @@
+package nerds.studiousTestProject.user.dto.oauth.signup;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import nerds.studiousTestProject.user.dto.general.MemberType;
+import nerds.studiousTestProject.user.dto.general.token.JwtTokenResponse;
+
+@Data
+@Builder
+public class OAuth2AuthenticateResponse {
+    private boolean exist;
+    private JwtTokenResponse jwtTokenResponse;
+    private UserInfo userInfo;
+
+    @Getter
+    @Setter
+    @Builder
+    public static class UserInfo {
+        Long providerId;
+        String email;
+        MemberType type;
+    }
+}
