7d0e58a8e7b393f04f7f39f0cfeb234617586282
kmw2378 <rlarla677@gmail.com>
2023-06-28 15:52:16 +0900

    remove: 기존 memberService.login() 메소드에서 findByEmailAndPassword 메소드를 사용하지 않아 삭제

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
-    Optional<Member> findByEmailAndPassword(String email, String encode);
     Optional<Member> findByProviderId(Long providerId);
     boolean existsByProviderId(Long providerId);
     boolean existsByEmail(String email);
