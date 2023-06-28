ab2a741ec83e4516b19999c12046e475fdfa579e
kmw2378 <rlarla677@gmail.com>
2023-06-28 14:25:01 +0900

    feat: 예외 메시지 추가 - MISMATCH_EMAIL_OR_PASSWORD("아이디 혹은 비밀번호가 일치하지 않습니다.") - NOT_DEFAULT_TYPE_USER("소셜 연동 계정입니다. 소셜 로그인을 사용하여 로그인해주세요.")

--- src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
+++ src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
@@ -3,6 +3,7 @@ package nerds.studiousTestProject.user.exception.message;
 public enum ExceptionMessage {
     MISMATCH_EMAIL("이메일이 일치하지 않습니다."),
     MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다."),
+    MISMATCH_EMAIL_OR_PASSWORD("아이디 혹은 비밀번호가 일치하지 않습니다."),
     ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
     LOGOUT_USER("로그아웃된 회원입니다."),
     MISMATCH_USERNAME_TOKEN("아이디와 토큰값이 일치하지 않습니다."),
@@ -14,7 +15,8 @@ public enum ExceptionMessage {
     USER_NOT_FOUND("유저를 찾을 수 없습니다."),
     MISMATCH_TOKEN("토큰명이 일치하지 않습니다."),
     NOT_SUPPORTED_JWT("JWT 토큰이 지원하지 않습니다."),
-    NOT_EXPIRED_REFRESH_TOKEN("Refresh Token이 만료되지 않았습니다.");
+    NOT_EXPIRED_REFRESH_TOKEN("Refresh Token이 만료되지 않았습니다."),
+    NOT_DEFAULT_TYPE_USER("소셜 연동 계정입니다. 소셜 로그인을 사용하여 로그인해주세요.");
 
     private final String message;
 
