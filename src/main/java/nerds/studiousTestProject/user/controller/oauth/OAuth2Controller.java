53892ddadd005267295ea7306adc988553c0c05c
kmw2378 <rlarla677@gmail.com>
2023-06-28 14:55:15 +0900

    refactor: import 문 최적화

--- src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
+++ src/main/java/nerds/studiousTestProject/user/controller/oauth/OAuth2Controller.java
@@ -2,12 +2,10 @@ package nerds.studiousTestProject.user.controller.oauth;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nerds.studiousTestProject.user.dto.general.token.JwtTokenResponse;
+import nerds.studiousTestProject.user.dto.oauth.signup.OAuth2AuthenticateResponse;
 import nerds.studiousTestProject.user.service.oauth.OAuth2Service;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
