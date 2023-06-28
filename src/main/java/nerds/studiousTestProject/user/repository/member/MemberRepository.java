38b42005f33253236d833e3525777ee215593b07
kmw2378 <rlarla677@gmail.com>
2023-06-28 14:35:45 +0900

    feat: 메소드 추가 - findByEmailAndPassword : email, password 값을 통해 회원 정보를 찾는 메소드 - findByProviderId : 소셜 id 를 통해 회원 정보를 찾는 메소드

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -1,12 +1,14 @@
 package nerds.studiousTestProject.user.repository.member;
 
-import nerds.studiousTestProject.user.entity.Member;
+import nerds.studiousTestProject.user.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByEmailAndPassword(String email, String encode);
+    Optional<Member> findByProviderId(Long providerId);
     boolean existsByProviderId(Long providerId);
     boolean existsByEmail(String email);
 }
