b6cddb8caf6e90fdece6b53c00724a25527448f1
kmw2378 <rlarla677@gmail.com>
2023-06-29 21:14:10 +0900

    feat: 메소드 추가 - findByPhoneNumber : 전화번호를 통해 회원 정보를 찾는 메소드, 휴대폰 정보를 통해 해당 회원의 이메일을 찾을 때 사용된다. - existsByPhoneNumber : 회원 가입 시 해당 전화번호로 가입한 이력이 있는지 확인할 때 사용된다.

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByProviderId(Long providerId);
+    Optional<Member> findByPhoneNumber(String PhoneNumber);
     boolean existsByProviderId(Long providerId);
     boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
