5a7ecd717768af699765128cc8b72d9467246bcd
kmw2378 <rlarla677@gmail.com>
2023-06-29 23:10:36 +0900

    remove: 사용하지 않는 예외메시지 삭제

--- src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
+++ src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
@@ -4,7 +4,6 @@ public enum ExceptionMessage {
     MISMATCH_EMAIL("이메일이 일치하지 않습니다."),
     MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다."),
     MISMATCH_PHONE_NUMBER("전화번호가 일치하지 않습니다."),
-    MISMATCH_EMAIL_OR_PASSWORD("아이디 혹은 비밀번호가 일치하지 않습니다."),
     ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
     PHONE_NUMBER_ALREADY_EXIST("해당 전화번호로 가입한 계정이 이미 존재합니다."),
     LOGOUT_USER("로그아웃된 회원입니다."),
