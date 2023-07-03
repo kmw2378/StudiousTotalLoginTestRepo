1f14330ea76deadcfc260599d7d8f66f0b4571af
kmw2378 <rlarla677@gmail.com>
2023-07-03 14:48:48 +0900

    feat: 값이 null 인 경우 DEFAULT 를 대입해주는 메소드 추가

--- src/main/java/nerds/studiousTestProject/user/entity/member/MemberType.java
+++ src/main/java/nerds/studiousTestProject/user/entity/member/MemberType.java
@@ -1,5 +1,9 @@
 package nerds.studiousTestProject.user.entity.member;
 
 public enum MemberType {
-   DEFAULT, NAVER, KAKAO, GOOGLE
+   DEFAULT, NAVER, KAKAO, GOOGLE;
+
+   public static MemberType handle(MemberType type) {
+      return type == null ? MemberType.DEFAULT : type;
+   }
 }
