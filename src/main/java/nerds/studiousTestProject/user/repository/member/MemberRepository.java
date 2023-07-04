a02e46b57fc54ff4ecffec5bab9d578301cfed68
kmw2378 <rlarla677@gmail.com>
2023-07-04 13:55:38 +0900

    refactor: 메소드 수정
    - findByEmail 반환 타입을 List로 수정. 이메일은 유니크한 값이 아니므로 여러 개가 조회될 수 있으므로, List로 수정
    - 타입이 다르면 providerId 가 같을 수 있어 타입과 providerId 두 개로 조회하도록 수정

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -1,15 +1,15 @@
 package nerds.studiousTestProject.user.repository.member;
 
 import nerds.studiousTestProject.user.entity.member.Member;
+import nerds.studiousTestProject.user.entity.member.MemberType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByProviderId(Long providerId);
     Optional<Member> findByPhoneNumber(String PhoneNumber);
-    boolean existsByProviderId(Long providerId);
-    boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
 }
