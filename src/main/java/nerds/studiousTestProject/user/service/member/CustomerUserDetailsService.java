4cd4f47e9f511947f33c18c31eb75cda1367160d
kmw2378 <rlarla677@gmail.com>
2023-07-04 13:58:29 +0900

    refactor: 메소드 수정 - 기존 email값만 가지고 조회하는 것이 아닌 "이메일&타입" 형태의 값을 받게 수정하여 이를 구분자(&)로 슬라이스 후 조회 및 권한 부여를 하도록 수정

--- src/main/java/nerds/studiousTestProject/user/service/member/CustomerUserDetailsService.java
+++ src/main/java/nerds/studiousTestProject/user/service/member/CustomerUserDetailsService.java
@@ -2,13 +2,13 @@ package nerds.studiousTestProject.user.service.member;
 
 import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.user.entity.member.Member;
+import nerds.studiousTestProject.user.entity.member.MemberType;
 import nerds.studiousTestProject.user.exception.message.ExceptionMessage;
 import nerds.studiousTestProject.user.repository.member.MemberRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -19,7 +19,10 @@ public class CustomerUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(username)
+        String[] split = username.split("&");
+        String email = split[0];
+        MemberType type = MemberType.valueOf(split[1]);
+        return memberRepository.findByEmailAndType(email, type)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException(ExceptionMessage.USER_NOT_FOUND.message()));
     }
