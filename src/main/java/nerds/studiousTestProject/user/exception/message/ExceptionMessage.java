4bfca6e3ee05baea703922ec76fc5bc524044400
kmw2378 <rlarla677@gmail.com>
2023-07-04 14:32:14 +0900

    feat: 예외 메시지 추가 - 비밀번호가 없으면서 일반회원인 경우 보여줄 예외 메시지 추가

--- src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
+++ src/main/java/nerds/studiousTestProject/user/exception/message/ExceptionMessage.java
@@ -4,6 +4,7 @@ public enum ExceptionMessage {
     MISMATCH_EMAIL("이메일이 일치하지 않습니다."),
     MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다."),
     MISMATCH_PHONE_NUMBER("전화번호가 일치하지 않습니다."),
+    NOT_EXIST_PASSWORD("비밀번호가 존재하지 않습니다. 일반회원인 경우 비밀번호는 필수입니다."),
     ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
     PHONE_NUMBER_ALREADY_EXIST("해당 전화번호로 가입한 계정이 이미 존재합니다."),
     LOGOUT_USER("로그아웃된 회원입니다."),
