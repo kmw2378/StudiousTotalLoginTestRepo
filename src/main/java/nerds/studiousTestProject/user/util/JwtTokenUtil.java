ad6ed2dd6af21fcd33ea919720e978cd5be2a373
kmw2378 <rlarla677@gmail.com>
2023-07-11 15:44:20 +0900

    feat: JWT 토큰 생성 시 헤더, 페이로드에 필요한 key 값 상수 추가

--- src/main/java/nerds/studiousTestProject/user/util/JwtTokenUtil.java
+++ src/main/java/nerds/studiousTestProject/user/util/JwtTokenUtil.java
@@ -8,8 +8,11 @@ public class JwtTokenUtil {
     public static final Long REISSUE_EXPIRE_TIME = 1000L * 60 * 60 * 3;         // Reissue 만료 시간 (3시간)
 
     // header
+    public static final String ALG_KEY = "alg";
+    public static final String TYPE_KEY = "typ";
+    public static final String TYPE_VALUE = "JWT";
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String TOKEN_TYPE_REFRESH = "refresh_token";
-    public static final String CLAIMS_AUTH = "auth";
+    public static final String AUTHORITIES_KEY = "auth";
 }
