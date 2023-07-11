4c44f89a297d54ba02f9999d29b9574b96763395
kmw2378 <rlarla677@gmail.com>
2023-07-11 15:38:56 +0900

    refactor: RefreshToken 의 PK 타입이 Long 타입으로 수정된 것을 반영

--- src/main/java/nerds/studiousTestProject/user/repository/token/RefreshTokenRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/token/RefreshTokenRepository.java
@@ -3,7 +3,5 @@ package nerds.studiousTestProject.user.repository.token;
 import nerds.studiousTestProject.user.entity.token.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
 }
