1e0574f7a4befde2506f820ea7737c4f060ad370
kmw2378 <rlarla677@gmail.com>
2023-07-22 23:08:43 +0900

    feat: studycafe 에서 사용되는 해시태그 이름 열거체 추가 - 추후, 영어로 리펙토링 예정

---
+++ src/main/java/nerds/studiousTestProject/studycafe/entity/hashtag/HashtagName.java
@@ -0,0 +1,16 @@
+package nerds.studiousTestProject.studycafe.entity.hashtag;
+
+public enum HashtagName {
+    친절해요,
+    깨끗해요,
+    쾌적해요,
+    면접용,
+    역세권,
+    최신식,
+    접근성,
+    설명과동일,
+    조용해요,
+    갓성비,
+    집중잘돼요,
+    프라이빗;
+}
