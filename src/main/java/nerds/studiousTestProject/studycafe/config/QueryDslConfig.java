de0f197cae4b26576690a72de24f03985b217596
kmw2378 <rlarla677@gmail.com>
2023-07-22 23:35:27 +0900

    feat: QueryDSL 관련 설정 사항 클래스 추가

---
+++ src/main/java/nerds/studiousTestProject/studycafe/config/QueryDslConfig.java
@@ -0,0 +1,23 @@
+package nerds.studiousTestProject.studycafe.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * QueryDSL 설정 파일
+ *  query를 생성할 수 있도록 EntityManager 주입
+ */
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
