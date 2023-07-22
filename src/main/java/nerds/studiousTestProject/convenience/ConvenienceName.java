0c838ab8b7f9750c0908f2afcaf8948c7b5f0047
kmw2378 <rlarla677@gmail.com>
2023-07-22 23:09:03 +0900

    feat: studycafe, room 에서 사용되는 편의시설 이름 열거체 추가 - 추후, 영어로 리펙토링 예정

---
+++ src/main/java/nerds/studiousTestProject/convenience/ConvenienceName.java
@@ -0,0 +1,23 @@
+package nerds.studiousTestProject.convenience;
+
+public enum ConvenienceName {
+    PARKING,
+    PRINT,
+    ELEVATOR,
+    PURIFIER,
+    BLANKET,
+    BEVERAGE,
+    TABLET_PC,
+    READING_DESK,
+    WIFI,
+    NO_KIDS_ZONE,
+    HDMI,
+    MONITOR,
+    PEN,
+    BOARD,
+    BEAM,
+    POINTER,
+    POWER_SOCKET,
+    NOTEBOOK,
+    AIR_CONDITIONER
+}
